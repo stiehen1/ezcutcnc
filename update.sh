634dbeae9ba19fe35495ef19a6f7540da70bbd9c
Scott <scott.tiehen@gmail.com>
2026-03-16 11:26:59 -0700

    Use unzipNLS in update.sh for Replit Nix

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L https://github.com/stiehen1/ezcutcnc/archive/refs/heads/main.zip -o r.zip && unzip -o r.zip && cp -r ezcutcnc-main/. . && rm -rf ezcutcnc-main r.zip && echo "Update complete!"
+curl -L https://github.com/stiehen1/ezcutcnc/archive/refs/heads/main.zip -o r.zip && unzipNLS -o r.zip && cp -r ezcutcnc-main/. . && rm -rf ezcutcnc-main r.zip && echo "Update complete!"
