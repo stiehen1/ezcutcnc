f04a2199bfdf5116ff7d722ae0a1201a56a5fdcc
Scott <scott.tiehen@gmail.com>
2026-03-16 12:20:01 -0700

    Serve app at root / instead of /mentor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,8 +10,7 @@ import NotFound from "@/pages/not-found";
 function Router() {
   return (
     <Switch>
-      <Route path="/">{() => <Redirect to="/mentor" />}</Route>
-      <Route path="/mentor" component={Mentor} />
+      <Route path="/" component={Mentor} />
       <Route path="/catalog" component={Catalog} />
       <Route component={NotFound} />
     </Switch>
