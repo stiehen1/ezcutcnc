ebdf97eb1d9643b7987f08afe08dddc15198ebdf
Scott <scott.tiehen@gmail.com>
2026-03-16 11:30:26 -0700

    Use python3 to unzip in update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -L https://github.com/stiehen1/ezcutcnc/archive/refs/heads/main.zip -o r.zip && unzipNLS -o r.zip && cp -r ezcutcnc-main/. . && rm -rf ezcutcnc-main r.zip && echo "Update complete!"
+curl -L https://github.com/stiehen1/ezcutcnc/archive/refs/heads/main.zip -o r.zip && python3 -c "import zipfile; zipfile.ZipFile('r.zip').extractall('.')" && cp -r ezcutcnc-main/. . && rm -rf ezcutcnc-main r.zip && echo "Update complete!"
